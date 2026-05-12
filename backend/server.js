9d46546c12b897795f17f9e9ea49be8f32bc2f50
Aman Gour <amangour3@gmail.com>
2026-05-12 15:43:08 +0530

    Added CORS

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(
+  cors({
+    origin:
+      "https://taskmanagerassessment-production-984b.up.railway.app",
+    credentials: true,
+  })
+);
+
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
